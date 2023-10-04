324373fb1a481e24e399b2d13cf8da241de2936f
BrLopes3 <you@example.com>
2023-10-04 13:10:22 -0400

    first commit

---
+++ ConjugActionApp/ConjugActionApp/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  ConjugActionApp
+//
+//  Created by Bruno Lopes on 2023-10-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var txtInputVerb: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func btnVerbConjugationTouchUpInside(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
+    
+
+}
+
